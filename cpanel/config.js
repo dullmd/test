2138c458d3ab50f303089f4e3cef339928ab3ab9
dullmd <abdulazizi20251@gmail.com>
2026-02-05 12:29:14 -0800

    Update config.js

--- cpanel/config.js
+++ cpanel/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    TELEGRAM_TOKEN: '8150273212:AAFYnZxr16Toh_fLwfRGdHuTbbjfHWyFuhFaXko',
+    TELEGRAM_TOKEN: '8029801364:AAHpkifIKi0Jo3UO8RIVGmAXeUeSByTZc7s',
     PANEL_DOMAIN: 'https://host.xcasper.site',
     PANEL_API_KEY: 'ptla_JgpVs9YYlZ0nise70M1lNjBJtLRcHXzbbhA23euyyGf29T',
-    AUTHORIZED_ADMINS: [5582659539,6541701591,8297850382], // Telegram IDs
+    AUTHORIZED_ADMINS: [8029801364], // Telegram IDs
     GLOBAL_EGG: '15', // Egg ID
     GLOBAL_LOCATION: '1' // Location ID
 };
